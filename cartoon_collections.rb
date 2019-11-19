8d906f5378deb125401bbd06989842bb44f5de4d
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-19 05:59:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,9 +10,10 @@ def summon_captain_planet(array)
 end 
 
 def long_planeteer_calls(array)
-  array.
-  
+  array.select do |x|
+    x.length > 4 
 end
+end 
 
 def find_the_cheese# code an argument here
   # the array below is here to help
